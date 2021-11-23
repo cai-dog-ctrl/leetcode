317312c8eb6f1d0faf9b7a7b3f133e4b002147c7
cai-dog-ctrl <1296643805@qq.com>
2021-11-23 21:06:05 +0800

    2021/11/23

---
+++ maxProfit.go
@@ -0,0 +1,25 @@
+package main
+
+//func maxProfit(prices []int) int {
+//	mx,ans:=prices[0],0
+//	for i:=0;i<len(prices);i++{
+//		ans=max(ans,prices[i]-mx)
+//		mx=min(mx,prices[i])
+//	}
+//	return ans
+//}
+//func min(a,b int)int {
+//	if a<b{
+//		return a
+//	}
+//	return b
+//}
+//func max(a,b int)int{
+//	if a>b{
+//		return a
+//	}
+//	return b
+//}
+func main() {
+
+}
---
+++ maxScoreSightseeingPair.go
@@ -0,0 +1,22 @@
+package main
+
+//func maxScoreSightseeingPair(values []int) int {
+//	mx,ans:=values[0]+0,0
+//	for j:=1;j<len(values);j++{
+//		ans = max(ans, mx + values[j] - j)
+//		// 边遍历边维护
+//		mx = max(mx, values[j] + j)
+//
+//
+//	}
+//	return ans
+//}
+//func max(a,b int)int{
+//	if a>b{
+//		return a
+//	}
+//	return b
+//}
+func main() {
+
+}
