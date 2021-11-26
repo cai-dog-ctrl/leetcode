ee22cf1bcf8ffc00b8c15b1bb8d336e2fec48c99
cai-dog-ctrl <1296643805@qq.com>
2021-11-26 19:36:54 +0800

    2021/11/26

---
+++ searchBST.go
@@ -0,0 +1,19 @@
+package main
+
+//func searchBST(root *TreeNode, val int) *TreeNode {
+//	if root==nil{
+//		return nil
+//	}
+//	if root.Val==val{
+//		return root
+//	}
+//	if root.Val>val{
+//		return searchBST(root.Left,val)
+//	}
+//	return searchBST(root.Right,val)
+//
+//
+//}
+func main() {
+
+}
